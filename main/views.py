33b268eacbff86c59950b45d6ade67bcc80263bb
dantoper556 <120361032+dantoper556@users.noreply.github.com>
2023-01-09 22:32:14 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .models import Profile
+from .theme_check import theme_res2
+
+def leaderboard(request):
+    data = {}
+    data['test'] = []
+    for i in Profile.objects.all():
+        t = theme_res2(i)
+        cnt = 0
+        for j in range(len(t)):
+            cnt += t[j]['compl']
+        data['test'].append([cnt, str(i.user), int(i.user_id)])
+    data['test'].sort(reverse=True)
+
+    return render(request, "index.html", context=data)
+
+def user_stats(request, name):
+    data = {}
+    for i in Profile.objects.all():
+        if (int(i.user_id) == name):
+            data['name'] = str(i.user)
+    return render(request, "profile.html", context=data)
 
 # Create your views here.
