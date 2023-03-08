10f34690c960ab43997189aa8666e11117c90feb
dantoper556 <120361032+dantoper556@users.noreply.github.com>
2023-03-08 15:55:45 +0300

    Update forms.py

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class NewUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username", "password1")
-    
+        fields = ("username", "password1", "password2")
+
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         if commit: user.save()
         return user
+    
+class CreateProfile(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("judge_id",)
